0433a000b4771676756f0780ea1ff869fde55366
lawin115 <lawinlawin05@gmail.com>
2025-09-09 18:11:42 +0300

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,61 +1,20 @@
 import UIKit
 import Flutter
 import Firebase
-import FirebaseMessaging
-import UserNotifications
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
 
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        FirebaseApp.configure()
-        
-        // UNUserNotificationCenter delegate
-        UNUserNotificationCenter.current().delegate = self
-        
-        // Request notification authorization
-        let authOptions: UNAuthorizationOptions
-        if #available(iOS 14.0, *) {
-            authOptions = [.alert, .badge, .sound, .banner]
-        } else {
-            authOptions = [.alert, .badge, .sound]
-        }
-        UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions
-        ) { granted, error in
-            print("Permission granted: \(granted)")
-        }
-        
-        application.registerForRemoteNotifications()
-        
-        GeneratedPluginRegistrant.register(with: self)
-        
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // Firebase destpê bike
+    FirebaseApp.configure()
     
-    // MARK: - FCM Token
-    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        Messaging.messaging().apnsToken = deviceToken
-        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-    }
-}
-
-// UNUserNotificationCenterDelegate extension
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    // Receive foreground notifications
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                willPresent notification: UNNotification,
-                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.alert, .badge, .sound])
-    }
+    // Pluginên Flutter tomar bike
+    GeneratedPluginRegistrant.register(with: self)
     
-    // Handle notification tap
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse,
-                                withCompletionHandler completionHandler: @escaping () -> Void) {
-        completionHandler()
-    }
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
