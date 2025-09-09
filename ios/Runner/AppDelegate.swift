3c787dcb0f56a52ab97652dc653bd32cd35305c0
lawin115 <lawinlawin05@gmail.com>
2025-09-09 22:25:44 +0300

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,27 +4,16 @@ import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    
     // Start Firebase
     FirebaseApp.configure()
     
     // Register Flutter plugins
     GeneratedPluginRegistrant.register(with: self)
-
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // MARK: - Handle Apple Sign In / Deep Links
-  override func application(
-    _ app: UIApplication,
-    open url: URL,
-    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-  ) -> Bool {
-      return super.application(app, open: url, options: options)
   }
 }
